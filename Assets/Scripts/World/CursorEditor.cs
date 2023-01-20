94bbac68a02f08850d41e24695c0e5afc1ce93d5
Vyacheslav Utrobin <utrobinslavik@gmail.com>
2023-01-20 19:58:07 +0500

    cursor fixс

---
+++ Assets/Scripts/World/CursorEditor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CursorEditor : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D cursorTexture;
+    private CursorMode _cursorMode = CursorMode.Auto;
+    private Vector2 _hotSpot = Vector2.zero;
+    void OnMouseEnter() { 
+        Cursor.SetCursor(cursorTexture, _hotSpot, _cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, _cursorMode);
+    }
+}
